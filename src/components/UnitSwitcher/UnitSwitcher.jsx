e17ea43b665ec19b8088e47e0e6a4fbbf69e92ff
Ajobiewe Joseph <joseph4hacking@gmail.com>
2025-09-07 18:41:09 +0100

    made some progress with the units switcher: so i decided to make use of radio buttons for the options so that selecting say fahrenheit will deselect celcius and vice versa, also in order to make use of the checkmark icon for my checked state, i used appearance none on the radio button to disable the dafult styling then used background image on the checked pseudo class to bring in the checkmark icon

--- src/components/UnitSwitcher/UnitSwitcher.jsx
+++ src/components/UnitSwitcher/UnitSwitcher.jsx
@@ -5,18 +5,61 @@ import DropdownIcon from '../../assets/images/icon-dropdown.svg';
 import './UnitSwitcher.css';
 
 export default function UnitSwitcher() {
-  const [showUnits, setShowUnits] = useState(false);
+  const [showUnitsOptions, setShowUnitsOptions] = useState(false);
+
+  const toggleShowUnitsOptions = () => {
+    setShowUnitsOptions(!showUnitsOptions);
+  }
   
   return (
     <div className='units-switcher'>
-      <button type='button' className='units-switcher-button'>
+      <button type='button' className='units-switcher-button' onClick={toggleShowUnitsOptions}>
 	<img src={UnitsIcon} alt='units icon' />
 	<span>Units</span>
 	<img src={DropdownIcon} alt='dropdown icon' />
       </button>
-      <div className='units-switcher-options'>
-	
-      </div>
+      { showUnitsOptions && (
+	<div className='units-switcher-options'>
+	  Units Options are being shown
+	  
+	  <fieldset className=''>
+	    <legend>Temperature</legend>
+	    <div className='form-group'>
+	      <label htmlFor='celcius'>Celcius(C)</label>
+	      <input type='radio' name='temperature' id ='celcius' value='celcius' />
+	    </div>
+	    <div className='form-group'>
+	      <label htmlFor='fahrenheit'>Fahrenheit(F)</label>
+	      <input type='radio' name='temperature' id ='fahrenheit' value='fahrenheit' />
+	    </div>
+	  </fieldset>
+
+	  <fieldset className=''>
+	    <legend>Wind Speed</legend>
+	    <div className='form-group'>
+	      <label htmlFor='kmph'>km/h</label>
+	      <input type='radio' name='wind_speed' id ='kmph' value='kmph' />
+	    </div>
+	    <div className='form-group'>
+	      <label htmlFor='mph'>mph</label>
+	      <input type='radio' name='wind_speed' id ='mph' value='mph' />
+	    </div>
+	  </fieldset>
+
+	  <fieldset className=''>
+	    <legend>Precipitation</legend>
+	    <div className='form-group'>
+	      <label htmlFor='millimeters'>Millimeters</label>
+	      <input type='radio' name='precipitation' id ='millimeters' value='millimeters' />
+	    </div>
+	    <div className='form-group'>
+	      <label htmlFor='inches'>Inches(in)</label>
+	      <input type='radio' name='precipitation' id ='inches' value='inches' />
+	    </div>
+	  </fieldset>
+	</div>
+      )}
     </div>
   );
+  
 }
